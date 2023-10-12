0caa98090412cf4f64618660099b050059e9dab3
ErgonomicQ <ergonomicQ@gmail.com>
2023-10-12 12:33:44 -0700

    minor adjustment

--- src/components/ProfliePicture.js
+++ src/components/ProfliePicture.js
@@ -3,8 +3,16 @@ import React from 'react';
 import profileImage from '../assets/itme.jpg'; // Adjust the path based on your project structure
 import './ProfilePic.css';
 import '../styles/CustomStyle.css'
+
+
 const ProfilePicture = () => {
-    return <img src={profileImage} alt="Your Name" className="profile" />;
+    const imageStyles = {
+        width: '500px', // Adjust the width as needed
+        height: '500px', // Adjust the height as needed
+        borderRadius: '50%', // To make the image circular
+    }
+
+    return <img src={profileImage} alt="Your Name" style={imageStyles} />;
 };
 
 export default ProfilePicture;
