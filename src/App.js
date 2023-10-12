5a9a6e4168a16e5e497dbf239df1c8c71d47b2ad
ErgonomicQ <ergonomicQ@gmail.com>
2023-10-12 12:08:15 -0700

    changed routing, adjusted classes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <Router basename="/">
+    <Router>
 
       <Header />
       <Navigation />
--- src/components/ProfliePicture.js
+++ src/components/ProfliePicture.js
@@ -1,10 +1,10 @@
-// src/components/ProfilePicture.js
+
 import React from 'react';
 import profileImage from '../assets/itme.jpg'; // Adjust the path based on your project structure
 import './ProfilePic.css';
 
 const ProfilePicture = () => {
-    return <img src={profileImage} alt="Your Name" className="profile-picture" />;
+    return <img src={profileImage} alt="Your Name" className="profile" />;
 };
 
 export default ProfilePicture;
