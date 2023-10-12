6d5f17b53beb69704abab4ea7054e3e2eb56ff4b
ErgonomicQ <ergonomicQ@gmail.com>
2023-10-11 23:57:22 -0700

    changed routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Navigation from './components/Navigation';
 import AboutMe from './components/AboutMe';
