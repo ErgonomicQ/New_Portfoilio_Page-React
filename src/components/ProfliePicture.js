685a3047abfcabb5d5d05b446a5532b008312018
ErgonomicQ <ergonomicQ@gmail.com>
2023-10-12 12:28:45 -0700

    minor adjustment

--- src/components/ProfliePicture.js
+++ src/components/ProfliePicture.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import profileImage from '../assets/itme.jpg'; // Adjust the path based on your project structure
 import './ProfilePic.css';
-
+import '../styles/CustomStyle.css'
 const ProfilePicture = () => {
     return <img src={profileImage} alt="Your Name" className="profile" />;
 };
