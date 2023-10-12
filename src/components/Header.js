08fbf9adc3bf264831400c8ff8cbb177727d7282
ErgonomicQ <ergonomicQ@gmail.com>
2023-10-11 19:07:47 -0700

    adding profile pic component and styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
-
+import bannerImage  from '../assets/banner.png'
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header-content">
         <div className="left-content">
-          <img src="/path/to/your/banner-image.jpg" alt="Banner" className="banner-image" />
-          <h1 className="title">Your Name</h1>
+          <img src={bannerImage} alt="Banner" className="banner-image" />
+          <h1 className="title">
+            Cody Cooke
+          </h1>
         </div>
         <div className="right-content">
            
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import './Navigation.css' 
 
 const Navigation = () => {
     return(
         <nav className="navbar" role="navigation" aria-label="main-navigation">
             <div className="navbar-brand">
                 <Link className="navbar-item" to="/">
-                    <strong>Pages:</strong>
+                    <strong>Go To:</strong>
                 </Link>
             </div>
             <div className="navbar-menu">
